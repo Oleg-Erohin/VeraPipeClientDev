7a5ac98012f0cb091c57fcc338f4cc3d0692bb4f
Aviad Udi <aviadudi1@gmail.com>
2024-06-30 20:23:53 +0300

    - Update To Do list - Changed try catch in fetchData - After fetchData save to app state

--- src/models/IIsometric.ts
+++ src/models/IIsometric.ts
@@ -3,6 +3,7 @@ import { ICoordinate } from "./ICoordinate";
 export interface IIsometric{
     id: number;
     name: string;
+    //    pidsAndSheets1: { [key: string]: number[]; };
     pidsAndSheets: Map<string,number[]>;
     revision: string;
     date: Date; // string?
